b2421fca506f555fcc5faf1f3b6b7eb5aeaba156
cheikh <cheikh@cngroup.dk>
2018-05-02 14:25:29 +0200

    ControllerExceptionAdvice added

---
+++ src/main/java/dk/cngroup/trainings/spring/springassignment/controller/advice/ControllerExceptionsAdvice.java
@@ -0,0 +1,57 @@
+package dk.cngroup.trainings.spring.springassignment.controller.advice;
+
+import dk.cngroup.trainings.spring.springassignment.exception.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice(annotations = Controller.class)
+public class ControllerExceptionsAdvice {
+
+	@ExceptionHandler(value = AnimalNotFoundException.class)
+	public ResponseEntity<String> animalNotFound(Exception e) {
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(value = InvalidAnimalException.class)
+	public ResponseEntity<String> invalidAnimal(Exception e) {
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(value = CareTakerNotFoundException.class)
+	public ResponseEntity<String> careTakerNotFound(Exception e) {
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(value = InvalidCareTakerException.class)
+	public ResponseEntity<String> invalidCareTakerException(Exception e) {
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(value = AnimalAndCareTakerAlreadyLinked.class)
+	public ResponseEntity<String> animalAndCareTakerAlreadyLinked(Exception e) {
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(value = AnimalsListEmptyException.class)
+	public ResponseEntity<String> animalsListIsEmpty(Exception e) {
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(value = CareTakersListEmptyException.class)
+	public ResponseEntity<String> careTakersListIsEmpty(Exception e) {
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(value = AnimalNotFoundInCareTakerAnimalsListException.class)
+	public ResponseEntity<String> animalInCareNotFound(Exception e) {
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(value = CareTakerNotInTheAnimalCareTakersListException.class)
+	public ResponseEntity<String> careTakerNotTaking(Exception e) {
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+}
