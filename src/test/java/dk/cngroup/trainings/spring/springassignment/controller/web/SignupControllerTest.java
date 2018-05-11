d9c65b888f67d2bf3e55b0f8e64d6717464bcb95
cheikh <cheikh@cngroup.dk>
2018-05-11 16:49:06 +0200

    Login and Sign up using hashed password fixed. tests partially implemented for controllers.

--- src/test/java/dk/cngroup/trainings/spring/springassignment/controller/web/SignupControllerTest.java
+++ src/test/java/dk/cngroup/trainings/spring/springassignment/controller/web/SignupControllerTest.java
@@ -1,14 +1,15 @@
 package dk.cngroup.trainings.spring.springassignment.controller.web;
 
-import dk.cngroup.trainings.spring.springassignment.config.WebSecurityConfig;
+import dk.cngroup.trainings.spring.springassignment.controller.dto.user.UserSignupDTO;
+import dk.cngroup.trainings.spring.springassignment.controller.helper.UserDtoValidatorService;
 import dk.cngroup.trainings.spring.springassignment.service.user.UserService;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -16,31 +17,28 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.logout;
-import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {WebSecurityConfig.class})
+@ContextConfiguration
 @WebAppConfiguration
 @SpringBootTest
-public class LoginControllerTest {
-
-	@Autowired
-	private WebApplicationContext context;
+public class SignupControllerTest {
 
 	@MockBean
 	private UserService userService;
-
 	@MockBean
-	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	private UserDtoValidatorService userDtoValidatorService;
 
+	@Autowired
+	private WebApplicationContext context;
 	private MockMvc mockMvc;
 
 	@Before
-	public void setup() {
+	public void before() {
 		mockMvc = MockMvcBuilders
 				.webAppContextSetup(context)
 				.apply(springSecurity())
@@ -48,15 +46,26 @@ public class LoginControllerTest {
 	}
 
 	@Test
-	public void nonLoggedUserRedirectionTest() throws Exception {
-		mockMvc.perform(get("/web/animal/list"))
-				.andExpect(redirectedUrl("http://localhost/web/login"));
+	public void signupFormTest() throws Exception {
+		// Always happy path:
+		mockMvc.perform(get("/signup"))
+				.andExpect(view().name("views/authentication/signup"))
+				.andExpect(model().attribute("userSignupDto",
+						Matchers.notNullValue(UserSignupDTO.class)));
 	}
 
 	@Test
-	public void redirectLoggedOffUserTest() throws Exception {
-		mockMvc.perform(logout("/web/logout"))
-				.andExpect(redirectedUrl("/web/login?logout"))
-				.andExpect(unauthenticated());
+	public void signupValidUserTest() throws Exception {
+		// Happy path:
+
+		mockMvc.perform(post("/signup")
+				.param("firstName", "Walid")
+				.param("lastName", "cheikh")
+				.param("email", "walid@email.com")
+				.param("password", "1234")
+				.param("matchingPassword", "1234"))
+				.andExpect(redirectedUrl("/web/login"));
 	}
+
+	// Sad paths still need to be implemented.
 }
